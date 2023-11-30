73a4c898e606099cf61c9fc76727964954b9557f
kaseen <srbtrxtheinsane@gmail.com>
2023-11-30 17:34:41 +0100

    Deploy script working fully

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,16 +2,27 @@
 pragma solidity ^0.8.19;
 
 import { LuckySix, UUPSProxy } from 'src/LuckySix.sol';
+import { AutomationRegistrarInterface, RegistrationParams } from './AutomationRegistrarInterface.sol';
+import '@chainlink/automation/interfaces/v2_1/IKeeperRegistryMaster.sol';
 import '@chainlink/interfaces/VRFCoordinatorV2Interface.sol';
+import '@chainlink/shared/interfaces/LinkTokenInterface.sol';
 import 'forge-std/Script.sol';
 
 contract DeployScript is Script {
+
+    // VRF
     address immutable COORDINATOR_ADDRESS;
-    address immutable LINK_TOKEN;
     bytes32 immutable KEYHASH;
     uint64 immutable SUBSCRIPTION_ID;
 
+    // Automation
+    address immutable LINK_ADDRESS;
+    address immutable REGISTRY_ADDRESS;
+    address immutable REGISTRAR_ADDRESS;
+
     VRFCoordinatorV2Interface immutable COORDINATOR;
+    AutomationRegistrarInterface immutable REGISTRAR;
+    LinkTokenInterface immutable LINK_TOKEN;
     uint256 immutable PRIVATE_KEY;
 
     /**
@@ -23,29 +34,39 @@ contract DeployScript is Script {
             COORDINATOR_ADDRESS = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
             KEYHASH = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
             SUBSCRIPTION_ID = 1508;
-            LINK_TOKEN = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+            REGISTRY_ADDRESS = 0x86EFBD0b6736Bed994962f9797049422A3A8E8Ad;
+            REGISTRAR_ADDRESS = 0xb0E49c5D0d05cbc241d68c05BC5BA1d1B7B72976;
+            LINK_ADDRESS = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
         }
         // For Mumbai testnet
         else if(block.chainid == 80001){
             COORDINATOR_ADDRESS = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
             KEYHASH = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
             SUBSCRIPTION_ID = 6579;
-            LINK_TOKEN = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+            REGISTRY_ADDRESS = 0xf97C091179A4A4d666da7a2764dDeD4F932FC14A;
+            REGISTRAR_ADDRESS = 0xb58E509b59538256854b2a223289160F83B23F92;
+            LINK_ADDRESS = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
         }
         else
             revert();
 
         COORDINATOR = VRFCoordinatorV2Interface(COORDINATOR_ADDRESS);
+        REGISTRAR = AutomationRegistrarInterface(REGISTRAR_ADDRESS);
+        LINK_TOKEN = LinkTokenInterface(LINK_ADDRESS);
         PRIVATE_KEY = vm.envUint("PRIVATE_KEY");
     }
 
+    /**
+     * @dev Script deploying an upgradable contract, adding a VRF consumer, and automating with Chainlink keepers.
+     */
     function run() external {
         vm.startBroadcast(PRIVATE_KEY);
 
         address lotteryAddress = deployLottery();
         addVrfConsumer(lotteryAddress);
 
-        //addForwarder(0x5E6ee5534b4C7081E852788b1a8781EDFA1b35e5, 0xD0E80E2ceDCF8c8953358e5E3A9EBc873aB3A848);
+        uint256 keeperId = addKeeper(lotteryAddress, msg.sender, 5 * 10 ** 18);
+        addForwarder(lotteryAddress, keeperId);
 
         vm.stopBroadcast();
     }
@@ -70,10 +91,40 @@ contract DeployScript is Script {
     }
 
     /**
-     * @dev After the registration of upkeep, a unique `forwarder` address is created for permissioned
-     *      access to the `performUpkeep` function.
+     * @dev This function allows `amount` of LINK tokens to be funded for upkeep in the automation registrar
+     *      and registers the upkeep, returning the `upkeepId` when successful.
      */
-    function addForwarder(address lotteryAddress, address forwarderAddress) private {
+    function addKeeper(address lotteryAddress, address owner, uint256 amount) private returns (uint256) {
+        RegistrationParams memory params = RegistrationParams({
+            name: "KEEPER",
+            encryptedEmail: hex"",
+            upkeepContract: lotteryAddress,
+            gasLimit: uint32(500000),
+            adminAddress: owner,
+            triggerType: uint8(0),  // Conditional upkeep
+            checkData: hex"",
+            triggerConfig: hex"",
+            offchainConfig: hex"",
+            amount: uint96(amount)
+        });
+
+        LINK_TOKEN.approve(address(REGISTRAR), params.amount);
+        uint256 upkeepId = REGISTRAR.registerUpkeep(params);
+
+        if(upkeepId == 0) revert(); 
+
+        return upkeepId;
+    }
+
+    /**
+     * @dev This function retrieves a unique address from the upkeep with the given `upkeepId`, which is
+     *      then set as the `forwarderAddress` for the `onlyKeeper` modifier. This modifier allows
+     *      permissioned access for the `performUpkeep` function.
+     */
+    function addForwarder(address lotteryAddress, uint256 upkeepId) private {
+        IKeeperRegistryMaster registry = IKeeperRegistryMaster(REGISTRY_ADDRESS);
+        address forwarderAddress = registry.getForwarder(upkeepId);
+
         LuckySix game = LuckySix(payable(lotteryAddress));
         game.setKeeperAddress(forwarderAddress);
     }
